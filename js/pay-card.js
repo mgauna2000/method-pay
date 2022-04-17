5b2bf6622531fcdcde57b3533de9473bacde3ce8
Martin <martingauna2000@gmail.com>
2022-04-17 19:44:59 -0300

    js select form

--- js/pay-card.js
+++ js/pay-card.js
@@ -10,3 +10,22 @@ btnOpenForm.addEventListener("click", () => {
     btnOpenForm.classList.toggle("active");
     form.classList.toggle("active");
 });
+
+// select del mes dinamicamente
+for(let i = 1; i <= 12; i++) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerText = i;
+    form.selectMonth.appendChild(option);
+}
+// select del año dinamicamente
+
+// guardamos el año actual dentro de una variable
+const currentYear = new Date().getFullYear();
+
+for(let i = currentYear; i <= currentYear + 8; i++) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.innerText = i;
+    form.selectYear.appendChild(option);
+}
