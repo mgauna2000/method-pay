94d383b605418d74f26aa1b849db2c3c0aafb1b1
Martin <martingauna2000@gmail.com>
2022-04-18 14:41:49 -0300

    add data json

--- js/pay-card.js
+++ js/pay-card.js
@@ -4,6 +4,10 @@ const form = document.querySelector("#card-form");
 const numberCard = document.querySelector("#card .number");
 const nameCard = document.querySelector("#card .name");
 const logo = document.querySelector("#logo");
+const signature = document.querySelector("#card .signature p");
+const monthExpiration = document.querySelector("#card .month");
+const yearExpiration = document.querySelector("#card .year");
+const ccv = document.querySelector("#card .ccv");
 
 card.addEventListener("click", () => {
     card.classList.toggle("active");
@@ -13,6 +17,14 @@ btnOpenForm.addEventListener("click", () => {
     btnOpenForm.classList.toggle("active");
     form.classList.toggle("active");
 });
+
+// mostramos el frente de la tarjeta
+const seeFront = () => {
+    //comprobamos si tiene la clase active y si la tiene la borramos
+    if(card.classList.contains("active")) {
+        card.classList.remove("active");
+    }
+}
 
 // select del mes dinamicamente
 for(let i = 1; i <= 12; i++) {
@@ -68,4 +80,48 @@ form.inputNumber.addEventListener("keyup", (e) => {
         image.src = "../img/logos/mastercard.png";
         logo.appendChild(image);
     }
+
+    //damos vuelta la tarjeta para que el usuario vea el frente
+    seeFront();
+});
+
+//nombre de tarjeta
+form.inputName.addEventListener("keyup", (e) => {
+    let valueInput = e.target.value;
+    form.inputName.value = valueInput.replace(/[0-9]/g, '');
+    nameCard.textContent = valueInput;
+    signature.textContent = valueInput;
+
+    // si deja el input vacio que aparezca un nombre por defecto
+    if(valueInput == '') {
+        nameCard.textContent = "Juan Perez";
+    }
+    seeFront();
+});
+
+// mes
+form.selectMonth.addEventListener("change", (e) => {
+    monthExpiration.textContent = e.target.value;
+    seeFront();
+});
+
+//año
+form.selectYear.addEventListener("change", (e) => {
+    // con slice mostramos solo los 2 ultimos digitos
+    yearExpiration.textContent = e.target.value.slice(2);
+    seeFront();
+});
+
+// ccv 
+form.inputCCV.addEventListener("keyup", (e) => {
+    if(!card.classList.contains("active")) {
+        card.classList.toggle("active");
+    }
+    form.inputCCV.value = form.inputCCV.value
+     //eliminamos espacios en blanco usando una exprecion regular
+     .replace(/\s/g, '')
+     // eliminamos las letras
+     .replace(/\D/g, '');
+
+     ccv.textContent = form.inputCCV.value;
 });
